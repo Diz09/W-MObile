bd2acc679504fb1ab51b0c4d8ccf8a3afa9daecd
Dimas <izzulhaqzaindimad@gmail.com>
2023-09-13 21:52:07 +0700

    update

---
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnFirst, btnSecond;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFirst = (Button) findViewById(R.id.first);
+        btnSecond = (Button) findViewById(R.id.second);
+
+        btnFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new SecondFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.replace(R.id.framelayout, fragment);
+        ft.commit();
+    }
+}
---
+++ Fragment/app/src/main/java/com/example/fragment/SecondFragment.java
@@ -0,0 +1,30 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import android.app.Fragment;
+
+public class SecondFragment extends Fragment {
+    View view;
+    Button secondBtn;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        secondBtn = view.findViewById(R.id.secondButton);
+        secondBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "First Fragment", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
+    }
+}
+
